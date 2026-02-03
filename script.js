b7c44aa9d581e2e4a61b40a8dade6e11b3fe5f6e
Wolverrine-glitch <haydenorens2803@gmail.com>
2026-02-03 16:26:27 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function showSection(id) {
-  const sections = document.querySelectorAll('.intel-section');
-  sections.forEach(sec => sec.classList.remove('active'));
-
-  document.getElementById(id).classList.add('active');
-}
